06f2f78be36de84f0a6f341c7ad99833085fabfb
Phong Le <phong_le@epam.com>
2024-12-24 10:23:29 +0700

    Resolve cors issue for react app

--- src/main/java/com/learn/oct2024/profile_service/controller/AuthController.java
+++ src/main/java/com/learn/oct2024/profile_service/controller/AuthController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @Slf4j(topic = "AuthController")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     @Autowired
